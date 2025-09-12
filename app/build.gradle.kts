402fdd04f87dab17d5838eb68899eae0f70d51ef
blevatino <b.levatino@alumno.etec.um.edu.ar>
2025-09-12 11:36:46 -0300

    Paso 7 (Crear ControladorPrincipal)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation(libs.guava)
 
     //Kotlinx para leer archivos JSON
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     //Para comunicarse con python
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
